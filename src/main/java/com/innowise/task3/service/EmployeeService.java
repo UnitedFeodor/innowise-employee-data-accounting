4a68bf1978496145cac9d26f6a65fa5146d000fa
Feodor Kachinsky <fedor15zil@gmail.com>
2023-03-16 19:30:54 +0300

    added stub service

--- src/main/java/com/innowise/task3/service/EmployeeService.java
+++ src/main/java/com/innowise/task3/service/EmployeeService.java
@@ -1,4 +1,22 @@
 package com.innowise.task3.service;
 
+import com.innowise.task3.dto.*;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    List<EmployeeDTO> getAllEmployees();
+
+    EmployeeDTO getEmployeeWithId(int id);
+
+    void editEmployee(EditEmployeeDTO editEmployeeDTO);
+
+    void addEmployee(AddEmployeeDTO addEmployeeDTO);
+
+    void deleteEmployee(int id);
+
+    boolean login(LoginDTO loginDTO);
+    boolean register (RegisterDTO registerDTO);
+
 }
---
+++ src/main/java/com/innowise/task3/service/implementation/EmployeeServiceImplementation.java
@@ -0,0 +1,47 @@
+package com.innowise.task3.service.implementation;
+
+import com.innowise.task3.dao.DAOProvider;
+import com.innowise.task3.dao.EmployeeDAO;
+import com.innowise.task3.dto.*;
+import com.innowise.task3.service.EmployeeService;
+
+import java.util.List;
+
+public class EmployeeServiceImplementation implements EmployeeService {
+
+    EmployeeDAO employeeDAO = DAOProvider.getInstance().getEmployeeDAO();
+    @Override
+    public List<EmployeeDTO> getAllEmployees() {
+        return null;
+    }
+
+    @Override
+    public EmployeeDTO getEmployeeWithId(int id) {
+        return null;
+    }
+
+    @Override
+    public void editEmployee(EditEmployeeDTO editEmployeeDTO) {
+
+    }
+
+    @Override
+    public void addEmployee(AddEmployeeDTO addEmployeeDTO) {
+
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+
+    }
+
+    @Override
+    public boolean login(LoginDTO loginDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean register(RegisterDTO registerDTO) {
+        return false;
+    }
+}
