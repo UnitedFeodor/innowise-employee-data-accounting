686d9c595808454cce00c8466efc46f2ff7e434b
Feodor Kachinsky <fedor15zil@gmail.com>
2023-03-17 18:18:58 +0300

    implemented GetEmployeesExecutor

--- src/main/java/com/innowise/task3/controller/implementation/GetEmployeesExecutor.java
+++ src/main/java/com/innowise/task3/controller/implementation/GetEmployeesExecutor.java
@@ -1,15 +1,33 @@
 package com.innowise.task3.controller.implementation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.innowise.task3.controller.Command;
+import com.innowise.task3.controller.json.mapper.ObjectMapperProvider;
+import com.innowise.task3.dto.EmployeeDTO;
+import com.innowise.task3.service.EmployeeService;
+import com.innowise.task3.service.ServiceProvider;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class GetEmployeesExecutor implements Command {
+
+    private final ObjectMapper objectMapper = ObjectMapperProvider.getInstance().getObjectMapper();
+    private final EmployeeService employeeService = ServiceProvider.getInstance().getEmployeeService();
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<EmployeeDTO> employeeList = employeeService.getAllEmployees();
+        String employeesJsonString = objectMapper.writeValueAsString(employeeList);
 
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(employeesJsonString);
+        out.flush();
     }
 }
